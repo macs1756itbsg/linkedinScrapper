d913c74eb7184ba9a4bf5456af4c1c2de9f97d0d
macs1756itbsg <maksym@itbsg.eu>
2026-01-27 14:46:26 +0200

    Added new linkedins

--- bind.js
+++ bind.js
@@ -9,7 +9,7 @@ const readUsers = () => {
 
 function getRandomNumber() {
   const min = 10_000;
-  const max = 50_000;
+  const max = 40_000;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
