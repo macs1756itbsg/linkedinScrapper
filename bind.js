dcb4ded1f12cfc2442d99647d3934c2edf8420d3
macs1756itbsg <maksym@itbsg.eu>
2026-02-03 16:23:59 +0200

    new

--- bind.js
+++ bind.js
@@ -10,7 +10,7 @@ const readUsers = () => {
 
 function getRandomNumber() {
   const min = 10_000;
-  const max = 60_000;
+  const max = 40_000;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
